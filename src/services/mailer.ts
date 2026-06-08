51a44137e75f4945f28569fc534a932354ff2b87
ThiagoDoCodigo <161274680+ThiagoDoCodigo@users.noreply.github.com>
2026-06-07 22:43:57 -0300

    fix: EMAIL_PORT Number convert

--- src/services/mailer.ts
+++ src/services/mailer.ts
@@ -5,7 +5,7 @@ export class Mailer {
   private static transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: Number(process.env.EMAIL_PORT),
-    secure: process.env.EMAIL_PORT === "465",
+    secure: Number(process.env.EMAIL_PORT) === 465,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
